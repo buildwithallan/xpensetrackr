4772d05d0f38e97cf302d42d3e6e7f47de0ded98
codetalkertive <codetalkertive@gmail.com>
2021-10-17 03:35:02 +0100

    Fix Some Issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,6 @@ class DashboardController < ApplicationController
   	@tot_income = Income.where(income_date: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).sum(:monthly_income)
   	@tot_expense = Expense.where(expense_date: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month).sum(:amount)
   	@expenses = Expense.all.last(5)
-  	@available_funds = @tot_income.to_d - @tot_expense.to_d
+  	@available_funds = @tot_income - @tot_expense
   end
 end
